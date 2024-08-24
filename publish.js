f22952eb60f1fc875318946df1c1747a277f29b1
shroomjaks <x3n09@proton.me>
2024-08-24 15:35:03 +0000

    Update

--- publish.js
+++ publish.js
@@ -23,7 +23,6 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN)
 try {
     console.log('Started refreshing application (/) commands.')
 
-    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: [] })
     await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands })
 
     console.log('Successfully reloaded application (/) commands.')
