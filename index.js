86a3104e2c330aaa19f8db52dbb30e0375a20a7a
shroomjaks <x3n09@proton.me>
2024-08-25 18:01:51 +0000

    Update

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.GuildMessageReactions
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.GuildMembers
     ]
 })
 
