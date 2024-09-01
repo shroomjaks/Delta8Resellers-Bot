fbdb2b8415883f4ca024ef0596db55bc7ac72be7
shroomjaks <x3n09@proton.me>
2024-09-01 11:08:29 -0400

    Update

--- events/dealCheck.js
+++ events/dealCheck.js
@@ -38,29 +38,57 @@ module.exports = {
         const page = await client.browser.newPage()
 
         await page.goto('https://delta8resellers.com/', { waitUntil: 'domcontentloaded' })
+
+        // <div class="owl-item" style="width: 344.733px; margin-right: 10px;"><div class="col">
+		// 		<a class="bf-deals-content" href="https://delta8resellers.com/brand/maui-labs/">
+		// 			<div class="bf-card-header">
+		// 				<h3 class="bf-card-heading mb-0">Maui Labs</h3>
+		// 			</div>
+		// 			<div class="bf-card-content">
+		// 				<p><strong>Buy 1 Get 1 Free</strong> <br> Maui Labs Products</p>
+		// 			</div>
+		// 			<span class="btn btn-primary m-3 mt-auto">SHOP DEAL</span>
+		// 		</a>
+		// 	</div></div>
         
         const allDeals = await page.evaluate(function () {
-            const dealCards = document.querySelectorAll('a.bf-deals-content')
+            const owlItemActive = document.querySelectorAll('owl-item active')
+            const owlItem = document.querySelectorAll('owl-item')
 
-            // Extract href and content text
-            return Array.from(dealCards).map(function (card) {
-                const url = card.getAttribute('href')
-                const contentText = card.querySelector('.bf-card-content').innerText.trim()
-                return { url, contentText }
-            })
+            // Extract href and content text for both owl-item and owl-item active
+
+            const deals = []
+
+            for (const item of owlItem) {
+                const a = item.querySelector('a')
+                const content = item.querySelector('.bf-card-content')
+
+                deals.push({
+                    url: a.href,
+                    contentText: content.innerText
+                })
+            }
+
+            for (const item of owlItemActive) {
+                const a = item.querySelector('a')
+                const content = item.querySelector('.bf-card-content')
+
+                deals.push({
+                    url: a.href,
+                    contentText: content.innerText
+                })
+            }
+
+            return deals
         })
 
-        console.log('Deal check finished.')
+        console.log(`Deal check finished, found ${allDeals.length} deals`)
 
         // Check if there are any new deals
-        const newDeals = allDeals.filter(function (deal) {
-            return !deals.some(function (oldDeal) {
-                return oldDeal.url === deal.url
-            })
-        })
+        const newDeals = allDeals.filter(deal => !deals.some(d => d.url === deal.url))
 
         if (newDeals.length > 0) {
-            console.log('New deals found!')
+            console.log(`${newDeals.length} new deals found!`)
 
             // Notify the channel
             const dealZone = await client.channels.fetch('1279817915696152656')
