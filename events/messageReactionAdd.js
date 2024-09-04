c423e054da03b64f0f02d5e848934fb0f8daa265
shroomjaks <x3n09@proton.me>
2024-09-04 13:13:56 -0400

    Update

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -10,13 +10,10 @@ module.exports = {
      * @param {User} user 
      * @param {Client} client 
      */
-    execute: async function (reaction, user, client) {
+    execute: async function (reaction, user, details, client) {
         if (reaction.message.id !== '1277129869565759510') return
-        if (user.bot) return
 
-        console.log(client.username)
-
-        const mainGuild = client.guilds.cache.get('1276202598692818996')
+        const mainGuild = await client.guilds.fetch('1276202598692818996')
 
         const member = await mainGuild.members.fetch(user.id)
         const memberRole = await mainGuild.roles.fetch('1276298509255184394')
