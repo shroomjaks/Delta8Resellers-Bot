09892816625066141dddb350422b6560329f6e25
shroomjaks <x3n09@proton.me>
2024-09-04 13:09:30 -0400

    Update

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -14,7 +14,9 @@ module.exports = {
         if (reaction.message.id !== '1277129869565759510') return
         if (user.bot) return
 
-        const mainGuild = await client.guilds.fetch('1276202598692818996')
+        console.log(client.username)
+
+        const mainGuild = client.guilds.cache.get('1276202598692818996')
 
         const member = await mainGuild.members.fetch(user.id)
         const memberRole = await mainGuild.roles.fetch('1276298509255184394')
