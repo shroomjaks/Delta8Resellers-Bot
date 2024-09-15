7fe53da846d4b0eedc60642f1a5ef495e11ba298
shroomjaks <x3n09@proton.me>
2024-09-15 18:23:40 -0400

    Update

--- commands/online.js
+++ commands/online.js
@@ -2,6 +2,11 @@ const { ApplicationCommandOptionType, ChatInputCommandInteraction, Client, Embed
 
 const { getAverageColor } = require('fast-average-color-node')
 
+const JSONdb = require('simple-json-db')
+
+const online = new JSONdb(path.join(__dirname, '..', 'database', 'online.json'))
+const offline = new JSONdb(path.join(__dirname, '..', 'database', 'offline.json'))
+
 module.exports = {
     name: 'online',
     description: 'Tells you the time a user was last seen online and for how long.',
@@ -22,8 +27,8 @@ module.exports = {
     execute: async function (interaction, client) {
         const user = interaction.options.getUser('user')
 
-        const lastonline = client.online.get(user.id) || null
-        const lastoffline = client.offline.get(user.id) || null
+        const lastonline = online.get(user.id) || null
+        const lastoffline = offline.get(user.id) || null
 
         if (!lastonline || !lastoffline) {
             return await interaction.reply({ content: 'I have no data on this user.', ephemeral: true })
