ef658904b23749ebccc270edde23a8b9f970ddfb
shroomjaks <x3n09@proton.me>
2024-09-20 16:45:28 -0400

    Update

--- events/stockCheck.js
+++ events/stockCheck.js
@@ -15,31 +15,76 @@ module.exports = {
 
         const updateChannel = await client.channels.fetch('1276548924936814755')
 
-        for (const product of products) {
-            const page = await client.browser.newPage()
+        try {
+            for (const product of products) {
+                const page = await client.browser.newPage()
 
-            await page.goto(product.url, { waitUntil: 'domcontentloaded' })
+                await page.goto(product.url, { waitUntil: 'domcontentloaded' })
 
-            const stock = await page.$('.stock').catch(() => null)
-            const stockText = await page.$eval('.stock', element => element.innerText).catch(() => null)
-            const productTitleText = await page.$eval('.product_title', element => element.innerText)
+                const stock = await page.$('.stock').catch(() => null)
+                const stockText = await page.$eval('.stock', element => element.innerText).catch(() => null)
+                const productTitleText = await page.$eval('.product_title', element => element.innerText)
 
-            const stockedStrainValues = await page.$$eval('#pa_flavor option', elements => elements.map(element => element.value).filter(value => value !== ''))
-            const stockedStrainNames = await page.$$eval('#pa_flavor option', elements => elements.map(element => element.innerText).filter(name => name !== 'Choose an option'))
+                const stockedStrainValues = await page.$$eval('#pa_flavor option', elements => elements.map(element => element.value).filter(value => value !== ''))
+                const stockedStrainNames = await page.$$eval('#pa_flavor option', elements => elements.map(element => element.innerText).filter(name => name !== 'Choose an option'))
 
-            const unstockedStrains = product.allStrains.filter(strain => !stockedStrainValues.includes(strain.strainValue))
+                const unstockedStrains = product.allStrains.filter(strain => !stockedStrainValues.includes(strain.strainValue))
 
-            const oldStocked = product.stocked
+                const oldStocked = product.stocked
 
-            if (!stock) {
-                product.stocked = true
-            } else if (stock && stockText !== 'This product is currently out of stock and unavailable.') {
-                product.stocked = true
-            } else {
-                product.stocked = false
+                if (!stock) {
+                    product.stocked = true
+                } else if (stock && stockText !== 'This product is currently out of stock and unavailable.') {
+                    product.stocked = true
+                } else {
+                    product.stocked = false
+                }
+
+                if (product.stocked == true && oldStocked == false) {
+                    const embed = new EmbedBuilder()
+                        .setTitle(product.name)
+                        .setDescription(`This entire product has been restocked! 🎉`)
+                        .setThumbnail(product.imageUrl)
+                        .setURL(product.url)
+                        .setTimestamp(Date.now())
+                        .setColor('#05ef9d')
+
+                    await updateChannel.send({ embeds: [embed] })
+
+                    for (const userId of product.restockReminders) {
+                        const user = await client.users.fetch(userId)
+
+                        try {
+                            await user.send({ embeds: [embed] })
+                        } catch (error) {
+                            console.error(error)
+                        }
+                    }
+
+                    continue
+                } else if (product.stocked == false && oldStocked == true) {
+                    const embed = new EmbedBuilder()
+                        .setTitle(product.name)
+                        .setDescription(`This entire product is now out of stock. 😢`)
+                        .setThumbnail(product.imageUrl)
+                        .setURL(product.url)
+                        .setTimestamp(Date.now())
+                        .setColor('#FF0000')
+                        .setFooter({ text: 'Press the button below to be reminded when this product is restocked.' })
+
+                    const reminderButton = new ButtonBuilder()
+                        .setCustomId(`product:${product.uuid}`)
+                        .setLabel('Remind Me')
+                        .setStyle(ButtonStyle.Primary)
+
+                    const actionRow = new ActionRowBuilder()
+                        .addComponents(reminderButton)
+
+                    await updateChannel.send({ embeds: [embed], components: [actionRow] })
+                }
             }
 
-            console.info(`Product: ${productTitleText} Stocked: ${product.stocked}`)
+            console.log(`Product: ${productTitleText} Stocked: ${product.stocked}`)
 
             for (const strainValue of stockedStrainValues) {
                 await page.select('#pa_flavor', strainValue)
@@ -54,8 +99,6 @@ module.exports = {
                 const dbStock = product.strainStock.find(strain => strain.strainValue === strainValue)
 
                 dbStock.imageUrl = strainImage
-
-                if (dbStock.stock === strainStockAmount) continue
 
                 if (dbStock.stock === 0 && strainStockAmount > 1) {
                     const embed = new EmbedBuilder()
@@ -99,7 +142,7 @@ module.exports = {
 
                 console.log(`Strain: ${strain.strainName}, Amount: 0`)
 
-                if (dbStock.stock === 0) continue 
+                if (dbStock.stock === 0) continue
 
                 dbStock.stock = 0
 
@@ -124,53 +167,14 @@ module.exports = {
             }
 
             await page.close()
-
-            if (oldStocked === product.stocked) continue
-
-            if (product.stocked === true) {
-                const embed = new EmbedBuilder()
-                    .setTitle(product.name)
-                    .setDescription(`This entire product has been restocked! 🎉`)
-                    .setThumbnail(product.imageUrl)
-                    .setURL(product.url)
-                    .setTimestamp(Date.now())
-                    .setColor('#05ef9d')
-                
-                await updateChannel.send({ embeds: [embed] })
-
-                for (const userId of product.restockReminders) {
-                    const user = await client.users.fetch(userId)
-
-                    try {
-                        await user.send({ embeds: [embed] })
-                    } catch (error) {
-                        console.error(error)
-                    }
-                }
-            } else {
-                const embed = new EmbedBuilder()
-                    .setTitle(product.name)
-                    .setDescription(`This entire product is now out of stock. 😢`)
-                    .setThumbnail(product.imageUrl)
-                    .setURL(product.url)
-                    .setTimestamp(Date.now())
-                    .setColor('#FF0000')
-                    .setFooter({ text: 'Press the button below to be reminded when this product is restocked.' })
-
-                const reminderButton = new ButtonBuilder()
-                    .setCustomId(`product:${product.uuid}`)
-                    .setLabel('Remind Me')
-                    .setStyle(ButtonStyle.Primary)
-
-                const actionRow = new ActionRowBuilder()
-                    .addComponents(reminderButton)
-
-                await updateChannel.send({ embeds: [embed], components: [actionRow] })
-            }
+        } catch (error) {
+            console.log(error)
+        } finally {
+            client.stock.set('products', products)
+            console.log('Stock check complete.\n')
         }
 
-        client.stock.set('products', products)
 
-        console.log('Stock check complete.\n')
+
     }
 }
