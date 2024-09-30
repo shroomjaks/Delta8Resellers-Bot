ae47fd6d2a8e9909fd8c964f6b2ea1aaef606708
shroomjaks <x3n09@proton.me>
2024-09-30 15:54:21 -0400

    Update

--- events/redditCheck.js
+++ events/redditCheck.js
@@ -2,14 +2,8 @@ const { parseStringPromise } = require('xml2js')
 
 const { getAverageColor } = require('fast-average-color-node')
 
-const util = require('util')
-
-const cheerio = require('cheerio')
-
 const JSONdb = require('simple-json-db')
 const rss = new JSONdb('./database/rss.json')
-
-const url = 'https://www.reddit.com/r/delta8resellers/new/.json'
 
 module.exports = {
     event: 'redditCheck',
@@ -18,7 +12,7 @@ module.exports = {
     execute: async function (client) {
         const redditChannel = await client.channels.fetch('1290122497186074706')
 
-        const feed = await fetch(url)
+        const feed = await fetch('https://www.reddit.com/r/delta8resellers/new/.json')
         const json = await feed.json()
 
         for (const post of json.data.children) {
@@ -33,28 +27,18 @@ module.exports = {
 
             const color = await getAverageColor(avatarUrl)
 
-            if (post.data.selftext.length >= 1) {
-                const embed = new EmbedBuilder()
-                    .setTitle(post.data.title)
-                    .setDescription(post.data.selftext)
-                    .setAuthor({ name: username, iconURL: avatarUrl, url: `https://reddit.com/user/${username}` })
-                    .setURL(`https://www.reddit.com${post.data.permalink}`)
-                    .setThumbnail(post.data?.thumbnail)
-                    .setColor(color.hex)
-                redditChannel.send({ embeds: [embed] })
-            } else {
-                const embed = new EmbedBuilder()
-                    .setTitle(post.title[0])
-                    .setAuthor({ name: username, iconURL: authorInfo.data.icon_img, url: post.author[0].uri[0] })
-                    .setURL(post.link[0].$.href)
-                    .setThumbnail(post['media:thumbnail']?.[0]?.['$']?.url)
-                    .setColor(color.hex)
-                    .setTimestamp(Date.parse(post.published[0]))
+            const embed = new EmbedBuilder()
+                .setTitle(post.data.title)
+                .setAuthor({ name: username, iconURL: avatarUrl, url: `https://reddit.com/user/${username}` })
+                .setURL(`https://www.reddit.com${post.data.permalink}`)
+                .setColor(color.hex)
 
-                redditChannel.send({ embeds: [embed] })
-            }
+            if (post.data.selftext.length >= 1) embed.setDescription(post.data.selftext)
+            if (post.data?.thumbnail) embed.setThumbnail(post.data.thumbnail)
 
-            rss.set(post.id[0], true)
+            redditChannel.send({ embeds: [embed] })
+
+            rss.set(post.data.id, true)
         }
     }
 }
